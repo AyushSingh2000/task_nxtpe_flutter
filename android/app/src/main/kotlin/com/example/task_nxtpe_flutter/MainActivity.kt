0e99307ba591c4f94478f10bb33b24d920bdd9ec
Ayush Singh <awesomeayushawesome@gmail.com>
2024-03-16 13:56:33 +0530

    final commit

--- android/app/src/main/kotlin/com/example/task_nxtpe_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/task_nxtpe_flutter/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.task_nxtpe_flutter
 
+
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val channelName="Androidchannel";
-    override fun configureFlutterEngine(flutterEngine:FlutterEngine){
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine){
         super.configureFlutterEngine(flutterEngine)
 
-        var channel=MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName);
+        var channel= MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName);
 
         channel.setMethodCallHandler{ call, result->
             if(call.method=="showToast"){
